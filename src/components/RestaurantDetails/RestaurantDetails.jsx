6e425f5b0ce12ef60bf52f87ffb36ac16791f952
Michael O'Donnell <gorillaejones@gmail.com>
2019-09-10 14:56:58 -0500

    details appearing on dom

--- src/components/RestaurantDetails/RestaurantDetails.jsx
+++ src/components/RestaurantDetails/RestaurantDetails.jsx
@@ -5,12 +5,35 @@ import {connect} from 'react-redux';
 
 class RestaurantDetails extends Component {
     render() {
+        let restaurantArray = this.props.details.map(restaurant => {
+            return (
+                <div className="restaurantDetails">
+                    <h1>{restaurant.name}</h1>
+                    <p>{restaurant.type}</p>
+                    <img src={restaurant.photo_url} />
+                    <p>{restaurant.address}</p>
+                    <p>{restaurant.city}</p>
+                    <p>{restaurant.state}</p>
+                    <p>{restaurant.zip}</p>
+                    <button>Comment</button>
+                    <button>Closed?</button>
+                    <p>{restaurant.username} says: {restaurant.recommended}</p>
+                    <p>{restaurant.comment}</p>
+                </div>
+            )
+        })
         return (
             <div>
-                <p>Hello from details</p>
+                {/* {JSON.stringify(this.props.details)} */}
+                {restaurantArray}
             </div>
         );
     }
 }
 
-export default RestaurantDetails;
+const mapStateToProps = (reduxStore) => {
+    return {
+        details: reduxStore.detailsReducer
+    }
+}
+export default connect(mapStateToProps)(RestaurantDetails);
